79ba890b6a1a5b77e162df19c6668f96a02e6267
Rickie Zheng <rqzheng2008@126.com>
2023-11-24 14:04:54 +0800

    feat: add support for Chrome newer than V40

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,29 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
 export default {
-  input: ["src/main-a.js", "src/main-b.js"],
-  output: [
-    // ES module version, for modern browsers
-    {
-      dir: "public/module",
-      format: "es",
-      sourcemap: true
-    },
-    // SystemJS version, for older browsers
-    {
-      dir: "public/nomodule",
-      format: "system",
-      sourcemap: true
-    }
-  ]
+    input: ["src/main-a.js", "src/main-b.js"],
+    output: [
+        // ES module version, for modern browsers
+        {
+            dir: "public/module",
+            format: "es",
+            sourcemap: true
+        },
+        // SystemJS version, for older browsers
+        {
+            dir: "public/nomodule",
+            format: "system",
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        resolve(),
+        babel({
+            presets: ['@babel/preset-env'],
+            "targets": {
+                "chrome": "40",
+            }
+        }),
+    ],
 };
